7b2ebf9d70735d1501e14731222fbf002037ee50
marvin evins <maevins@cps.edu>
2016-09-11 07:58:10 -0500

    app working with push notifications

---
+++ Pods/Target Support Files/Pods-pushMyNotifs/Pods-pushMyNotifs-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_pushMyNotifsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_pushMyNotifsVersionString[];
+
---
+++ Pods/Target Support Files/Pods-pushMyNotifsTests/Pods-pushMyNotifsTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_pushMyNotifsTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_pushMyNotifsTestsVersionString[];
+
